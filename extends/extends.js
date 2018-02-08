3276964879c88bebef31043b4dc37b364f580057
Jialiang T <rememhe@outlook.com>
2018-02-08 17:15:00 +0800

    format

--- extends/extends.js
+++ extends/extends.js
@@ -1,30 +1,30 @@
 'use strict';
 
 function Super(name) {
-	this.name = name;
-	this.colors = ["red", "blue"];
+    this.name = name;
+    this.colors = ["red", "blue"];
 }
 
-Super.prototype.sayName = function() {
-	console.log(this.name);
+Super.prototype.sayName = function () {
+    console.log(this.name);
 };
 
 function Sub(name, age) {
-	Super.call(name);
-	this.age = age;
+    Super.call(name);
+    this.age = age;
 }
 
 inherits(Sub, Super);
 
-Sub.prototype.sayAge = function() {
-	console.log(this.age);
+Sub.prototype.sayAge = function () {
+    console.log(this.age);
 };
 
 function inherits(subType, superType) {
-	var F = function () {};
-	F.prototype = superType.prototype;
-	subType.prototype = new F();
-	subType.prototype.constructor = subType;
+    var F = function () {};
+    F.prototype = superType.prototype;
+    subType.prototype = new F();
+    subType.prototype.constructor = subType;
 }
 
 class Animal {
@@ -37,7 +37,7 @@ class Cat extends Animal {
     constructor(name) {
         super(name);
     }
-    
+
     say() {
         return "Hello, " + this.name;
     }
